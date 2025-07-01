f49dac9433386d2ccc1a05dd24507c34ee070361
HIBIKI_5201 <takuto5201@gmail.com>
2025-07-02 07:18:51 +0900

    [update]外部から追加できるように

--- Runtime/Debug/SymphonyDebugHUD.cs
+++ Runtime/Debug/SymphonyDebugHUD.cs
@@ -4,33 +4,25 @@ using UnityEngine.Profiling;
 
 namespace SymphonyFrameWork.Debugger
 {
-    /// <summary>
-    ///     ゲーム画面上にデバッグ情報を表示するクラス。
-    /// </summary>
     public class SymphonyDebugHUD : MonoBehaviour
     {
-        /// <summary>
-        ///     デバッグHUDに追加のテキストを表示します。
-        ///     LateUpdateで呼び出すと、前のフレームのテキストがクリアされるので注意してください。
-        /// </summary>
-        /// <param name="text"></param>
         public void AddText(string text)
         {
             _extraText += text + "\n";
         }
 
         private float deltaTime = 0.0f;
-
         private string _extraText = string.Empty;
+        private string _textToDisplay = string.Empty;
 
         private void Update()
         {
-            // フレーム時間の加算
             deltaTime += (Time.unscaledDeltaTime - deltaTime) * 0.1f;
-        }
 
-        private void LateUpdate()
-        {
+            _textToDisplay = GetProfilingText();
+            _textToDisplay += _extraText;
+
+            // _extraTextはUpdateでクリアする（OnGUI中にリセットしない）
             _extraText = string.Empty;
         }
 
@@ -38,20 +30,25 @@ namespace SymphonyFrameWork.Debugger
         {
             int w = Screen.width, h = Screen.height;
 
-            GUIStyle style = new GUIStyle();
+            Rect rect = new Rect(10, 10, w, h);
 
-            Rect rect = new Rect(10, 10, w, h * 2 / 100);
+            GUIStyle style = new GUIStyle();
             style.alignment = TextAnchor.UpperLeft;
             style.fontSize = h * 1 / 50;
             style.normal.textColor = Color.white;
 
+            GUI.Label(rect, _textToDisplay, style);
+        }
+
+        private string GetProfilingText()
+        {
+            // OnGUIで使用する文字列をここで確定しておく
             float msec = deltaTime * 1000.0f;
             float fps = 1.0f / deltaTime;
 
-            long totalMemory = GC.GetTotalMemory(false); // 管理メモリ（GCの影響受ける）
-            long monoMemory = Profiler.GetMonoUsedSizeLong(); // Mono管理メモリ
-            long totalAllocated = Profiler.GetTotalAllocatedMemoryLong(); // 全体の割り当て
-            long totalReserved = Profiler.GetTotalReservedMemoryLong(); // 予約済み
+            long monoMemory = Profiler.GetMonoUsedSizeLong();
+            long totalAllocated = Profiler.GetTotalAllocatedMemoryLong();
+            long totalReserved = Profiler.GetTotalReservedMemoryLong();
 
             string text = string.Format(
                 "FPS: {0:0.} ({1:0.0} ms)\n" +
@@ -62,9 +59,9 @@ namespace SymphonyFrameWork.Debugger
                 (monoMemory / (1024 * 1024)),
                 (totalAllocated / (1024 * 1024)),
                 (totalReserved / (1024 * 1024))
-                );
+            );
 
-            GUI.Label(rect, text, style);
+            return text;
         }
     }
 }
