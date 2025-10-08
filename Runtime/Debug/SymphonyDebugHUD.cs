d209a731665ce0238342b2153d6ca2854a8e39d6
HIBIKI_5201 <takuto5201@gmail.com>
2025-10-08 12:48:34 +0900

    [update]StringBuilderを使用して最適化

--- Runtime/Debug/SymphonyDebugHUD.cs
+++ Runtime/Debug/SymphonyDebugHUD.cs
@@ -1,6 +1,8 @@
-﻿using SymphonyFrameWork.System;
+﻿using SymphonyFrameWork.Core;
+using SymphonyFrameWork.System;
 using System;
 using System.Text;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.Profiling;
 
@@ -12,12 +14,37 @@ namespace SymphonyFrameWork.Debugger
     [DefaultExecutionOrder(1000)]
     public class SymphonyDebugHUD : MonoBehaviour
     {
+        [MenuItem(SymphonyConstant.TOOL_MENU_PATH + nameof(SymphonyDebugHUD) + nameof(Show))]
+        public static void Show()
+        {
+            _ = _debugHUD.Value; // アクセスしてインスタンスを作成。
+        }
+
+        [MenuItem(SymphonyConstant.TOOL_MENU_PATH + nameof(SymphonyDebugHUD) + nameof(Hide))]
+        public static void Hide()
+        {
+            if (_debugHUD.IsValueCreated) // 既にインスタンスが作成されている場合のみ。
+            {
+                // インスタンスを破棄して非表示にする。
+                Destroy(_debugHUD.Value.gameObject);
+                _debugHUD = new Lazy<SymphonyDebugHUD>(CreateDebugHUD);
+            }
+        }
+
         /// <summary>
         ///     SymphonyDebugHUDに追加のテキストを追加する
         /// </summary>
         /// <param name="text"></param>
-        public static void AddText(string text)
+        public static void AddText(string text, Color color = default)
         {
+            if (!_debugHUD.IsValueCreated) return; // インスタンスが作成されていない場合は何もしない。
+
+            if (color != default) //カラーを指定する
+            {
+                string colorHex = ColorUtility.ToHtmlStringRGB(color);
+                text = $"<color=#{colorHex}>{text}</color>";
+            }
+
             _debugHUD.Value._extraText.AppendLine(text);
         }
 
@@ -34,7 +61,7 @@ namespace SymphonyFrameWork.Debugger
         }
 
         private float _deltaTime = 0.0f;
-        private StringBuilder _extraText = null;
+        private StringBuilder _extraText = new();
         private StringBuilder _textToDisplay = null;
 
         private Rect _rect;
@@ -60,7 +87,8 @@ namespace SymphonyFrameWork.Debugger
             _deltaTime += (Time.unscaledDeltaTime - _deltaTime) * 0.1f; // デルタタイムの計算（タイムスケールに影響しない）
 
             //基本テキストを取得。
-            _textToDisplay = new(GetProfilingText());
+            _textToDisplay = new();
+            GetProfilingText(ref _textToDisplay);
 
             // 追加テキストを追加。
             _textToDisplay.AppendLine(_extraText.ToString());
@@ -68,7 +96,7 @@ namespace SymphonyFrameWork.Debugger
             _extraText = new();
         }
 
-        private string GetProfilingText()
+        private void GetProfilingText(ref StringBuilder text)
         {
             float msec = _deltaTime * 1000.0f; // ミリ秒に変換。
             float fps = 1.0f / _deltaTime; // FPSの計算。
@@ -77,18 +105,10 @@ namespace SymphonyFrameWork.Debugger
             long totalAllocated = Profiler.GetTotalAllocatedMemoryLong(); // 総アロケートメモリ量を取得。
             long totalReserved = Profiler.GetTotalReservedMemoryLong(); // 総リザーブメモリ量を取得。
 
-            string text = string.Format(
-                "FPS: {0:0.} ({1:0.0} ms)\n" +
-                "Mono Memory: {2} MB\n" +
-                "Total Allocated: {3} MB\n" +
-                "Total Reserved: {4} MB\n",
-                fps, msec,
-                (monoMemory / (1024 * 1024)),
-                (totalAllocated / (1024 * 1024)),
-                (totalReserved / (1024 * 1024))
-            );
-
-            return text;
+            text.AppendLine($"FPS: {fps:0.} ({msec:0.0} ms)");
+            text.AppendLine($"Mono Memory: {monoMemory / (1024 * 1024)} MB");
+            text.AppendLine($"Total Allocated: {totalAllocated / (1024 * 1024)} MB");
+            text.AppendLine($"Total Reserved: {totalReserved / (1024 * 1024)} MB");
         }
 
         private void OnGUI()
