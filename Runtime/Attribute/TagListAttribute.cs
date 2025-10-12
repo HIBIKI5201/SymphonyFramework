6233c875fc9a60a2253a999e52c6b7a0d3213b7a
HIBIKI_5201 <takuto5201@gmail.com>
2025-10-12 19:41:42 +0900

    [add]タグをリスト化するアトリビュートを追加

---
+++ Runtime/Attribute/TagListAttribute.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace SymphonyFrameWork.Attribute
+{
+    /// <summary>
+    ///     String変数をタグのリストから選択できるようにする。
+    /// </summary>
+    public class TagListAttribute
+    {
+        public TagListAttribute()
+        {
+        }
+    }
+}
