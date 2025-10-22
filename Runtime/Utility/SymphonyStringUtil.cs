9fd5da5278bcc733386c9237d1d3c865e4aceb31
HIBIKI_5201 <takuto5201@gmail.com>
2025-10-23 01:02:54 +0900

    [update]リッチテキストの文字にカラーを挿入するメソッドを追加

--- Runtime/Utility/SymphonyStringUtil.cs
+++ Runtime/Utility/SymphonyStringUtil.cs
@@ -1,9 +1,23 @@
-﻿namespace SymphonyFrameWork
+﻿using UnityEngine;
+
+namespace SymphonyFrameWork
 {
     /// <summary>
     ///     文字列のユーティリティクラス。
     /// </summary>
     public static class SymphonyStringUtil
     {
+        /// <summary>
+        ///     リッチテキストの文字にカラータグを挿入する。
+        /// </summary>
+        /// <param name="text"></param>
+        /// <param name="color"></param>
+        /// <returns></returns>
+        public static string AddRichTextColor(this string text, Color color)
+        {
+            // ColorをRGBに変換する。
+            string htmlColor = ColorUtility.ToHtmlStringRGB(color);
+            return $"<color={htmlColor}>{text}</color>"; // タグに入れて返す。
+        }
     }
 }
