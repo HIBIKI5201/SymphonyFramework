fd620e315f0a4a5f69468716ef08e9b460aea3bb
HIBIKI_5201 <takuto5201@gmail.com>
2026-02-26 05:51:07 +0900

    [add]データクラスを追加

---
+++ Runtime/System/SceneLoader/SceneLoadData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+
+namespace SymphonyFrameWork
+{
+    public class SceneLoadData
+    {
+        public SceneLoadData() { }
+
+        private static readonly Dictionary<string, Scene> _sceneDict = new();
+        private static readonly Dictionary<string, Action> _loadedActionDict = new();
+    }
+}
