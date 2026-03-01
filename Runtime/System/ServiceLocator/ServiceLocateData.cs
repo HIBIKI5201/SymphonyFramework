b6ca3408d4ac2c79f47301f71c92280b52816b3c
HIBIKI_5201 <takuto5201@gmail.com>
2026-03-01 17:51:38 +0900

    [update]データを追加

--- Runtime/System/ServiceLocator/ServiceLocateData.cs
+++ Runtime/System/ServiceLocator/ServiceLocateData.cs
@@ -1,19 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
 using UnityEngine;
 
-namespace SymphonyFrameWork
+namespace SymphonyFrameWork.System.ServiceLocate
 {
-    public class ServiceLocateData : MonoBehaviour
+    public class ServiceLocateData
     {
-        // Start is called once before the first execution of Update after the MonoBehaviour is created
-        void Start()
+        public ServiceLocateData()
         {
-        
+            _gameObject = new GameObject("ServiceLocateData");
+            SymphonyCoreSystem.MoveObjectToSymphonySystem(_gameObject);
         }
 
-        // Update is called once per frame
-        void Update()
-        {
-        
-        }
+        public GameObject Instance => _gameObject;
+        public Dictionary<Type, object> SingletonObjects => _singletonObjects;
+        public Dictionary<Type, Action> WaitingActions => _waitingActions;
+        public Dictionary<Type, Delegate> WaitingActionsWithInstance => _waitingActionsWithInstance;
+
+        [Tooltip("登録されているインスタンスを型をキーにして保持する辞書")]
+        private readonly Dictionary<Type, object> _singletonObjects = new();
+
+        [Tooltip("インスタンス登録まで待機してから実行されるコールバックアクションを保持する辞書")]
+        private readonly Dictionary<Type, Action> _waitingActions = new();
+        [Tooltip("インスタンス登録まで待機し、登録されたインスタンスを引数として受け取るコールバックアクションを保持する辞書")]
+        private readonly Dictionary<Type, Delegate> _waitingActionsWithInstance = new();
+
+        private readonly GameObject _gameObject;
+
+        public bool IsValid() =>
+            Instance != null && Instance.activeInHierarchy && Instance.scene.isLoaded;
     }
 }
