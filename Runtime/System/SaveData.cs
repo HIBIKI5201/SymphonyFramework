cbb5bb3151c40ab6c59549abdf3f9e71b3b50869
HIBIKI_5201 <takuto5201@gmail.com>
2026-03-18 23:47:52 +0900

    [add]セーブデータ型を分離

---
+++ Runtime/System/SaveData.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace SymphonyFrameWork.System
+{
+    [Serializable]
+    public class SaveData<T>
+    {
+        public SaveData(T dataType, DateTime saveDate = default)
+        {
+            if (saveDate == default) { saveDate = DateTime.Now; }
+
+            SaveDate = saveDate.ToString("O");
+            MainData = dataType;
+        }
+
+        public string SaveDate { get; set; }
+        public T MainData { get; set; }
+
+        public static bool operator ==(SaveData<T> a, SaveData<T> b)
+        {
+            if (ReferenceEquals(a, null)) { return ReferenceEquals(b, null); }
+
+            if (a.MainData == null) { return true; }
+
+            return ReferenceEquals(a, b);
+        }
+
+        public static bool operator !=(SaveData<T> a, SaveData<T> b) => !(a == b);
+
+        public override bool Equals(object obj) => this == obj as SaveData<T>;
+
+        public override string ToString()
+        {
+            return $"SaveDate: {SaveDate}\nMainData:\n{MainData}";
+        }
+    }
+}
--- Runtime/System/SaveDataSystem.cs
+++ Runtime/System/SaveDataSystem.cs
@@ -52,37 +52,4 @@ namespace SymphonyFrameWork.System
             _saveData = _loader.Load();
         }
     }
-
-    [Serializable]
-    public class SaveData<T>
-    {
-        public SaveData(T dataType, DateTime saveDate = default)
-        {
-            if (saveDate == default) { saveDate = DateTime.Now; }
-
-            SaveDate = saveDate.ToString("O");
-            MainData = dataType;
-        }
-
-        public string SaveDate { get; set; }
-        public T MainData { get; set; }
-
-        public static bool operator ==(SaveData<T> a, SaveData<T> b)
-        {
-            if (ReferenceEquals(a, null)) { return ReferenceEquals(b, null); }
-
-            if (a.MainData == null) { return true; }
-
-            return ReferenceEquals(a, b);
-        }
-
-        public static bool operator !=(SaveData<T> a, SaveData<T> b) => !(a == b);
-
-        public override bool Equals(object obj) => this == obj as SaveData<T>;
-
-        public override string ToString()
-        {
-            return $"SaveDate: {SaveDate}\nMainData:\n{MainData}";
-        }
-    }
 }
