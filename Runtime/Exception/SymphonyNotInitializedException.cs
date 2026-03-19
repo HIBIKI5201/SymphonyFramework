d20c2db1bf4ef1c911ee843a9f2490dd7bdd0d16
HIBIKI_5201 <takuto5201@gmail.com>
2026-03-19 13:27:10 +0900

    [update]タイプを受け入れるコンストラクタを追加

--- Runtime/Exception/SymphonyNotInitializedException.cs
+++ Runtime/Exception/SymphonyNotInitializedException.cs
@@ -7,9 +7,15 @@ namespace SymphonyFrameWork.Exceptions
         public SymphonyNotInitializedException() : base("SymphonyFrameworkが未初期化です。")
         {
         }
+
+        public SymphonyNotInitializedException(Type type) : base($"[{type.Name}] SymphonyFrameworkが未初期化です。")
+        {
+        }
+
         public SymphonyNotInitializedException(string message) : base(message)
         {
         }
+
         public SymphonyNotInitializedException(string message, Exception innerException) : base(message, innerException)
         {
         }
