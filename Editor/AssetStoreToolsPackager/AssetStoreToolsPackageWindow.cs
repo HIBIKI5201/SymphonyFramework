efa198f028d5465f383c146bf66a4cbfdb16238f
HIBIKI_5201 <takuto5201@gmail.com>
2026-03-23 18:02:22 +0900

    [update]ウィンドウでセレクトできるように

--- Editor/AssetStoreToolsPackager/AssetStoreToolsPackageWindow.cs
+++ Editor/AssetStoreToolsPackager/AssetStoreToolsPackageWindow.cs
@@ -1,5 +1,7 @@
 ﻿using SymphonyFrameWork.Core;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using UnityEditor;
 using UnityEngine;
 
@@ -20,20 +22,87 @@ namespace SymphonyFrameWork.Editor
             GetWindow<AssetStoreToolsPackageWindow>(false, "Asset Store Tools Packager", true);
         }
 
-        private string[] _directories;
-
-        private void OnEnable()
+        private class DirectoryItem
         {
-            // アセットごとのパスを生成。
-            _directories = Directory.GetDirectories(EditorSymphonyConstant.ASSET_STORE_TOOLS_PATH);
+            public string Path;
+            public string Name;
+            public bool IsSelected;
         }
+
+        private List<DirectoryItem> _directoryItems = new();
+        private Vector2 _scrollPosition;
 
         private void OnGUI()
         {
             GUILayout.Label("Asset Store Tools Packager", EditorStyles.boldLabel);
-            if (GUILayout.Button("Export Asset Store Tools Folder"))
+            EditorGUILayout.Space();
+
+            if (GUILayout.Button("Refresh", GUILayout.Width(100)))
             {
-                AssetStoreToolsPackager.Export(_directories);
+                RefreshDirectories();
+            }
+
+            EditorGUILayout.Space();
+
+            // 一括選択・解除
+            EditorGUILayout.BeginHorizontal();
+            if (GUILayout.Button("Select All", GUILayout.Width(100)))
+            {
+                _directoryItems.ForEach(d => d.IsSelected = true);
+            }
+            if (GUILayout.Button("Deselect All", GUILayout.Width(100)))
+            {
+                _directoryItems.ForEach(d => d.IsSelected = false);
+            }
+            EditorGUILayout.EndHorizontal();
+
+            EditorGUILayout.Space();
+
+            // ディレクトリ一覧
+            _scrollPosition = EditorGUILayout.BeginScrollView(_scrollPosition, EditorStyles.helpBox);
+            foreach (DirectoryItem item in _directoryItems)
+            {
+                item.IsSelected = EditorGUILayout.ToggleLeft(item.Name, item.IsSelected);
+            }
+            EditorGUILayout.EndScrollView();
+
+            EditorGUILayout.Space();
+
+            // エクスポートボタン
+            using (new EditorGUI.DisabledGroupScope(_directoryItems.All(d => !d.IsSelected)))
+            {
+                if (GUILayout.Button("Export Selected Directories", GUILayout.Height(30)))
+                {
+                    string[] selectedDirs = _directoryItems
+                        .Where(d => d.IsSelected)
+                        .Select(d => d.Path)
+                        .ToArray();
+
+                    AssetStoreToolsPackager.Export(selectedDirs);
+                }
+            }
+        }
+
+        private void OnEnable()
+        {
+            RefreshDirectories();
+        }
+
+        private void RefreshDirectories()
+        {
+            _directoryItems.Clear();
+            if (AssetDatabase.IsValidFolder(EditorSymphonyConstant.ASSET_STORE_TOOLS_PATH))
+            {
+                string[] dirs = Directory.GetDirectories(EditorSymphonyConstant.ASSET_STORE_TOOLS_PATH);
+                foreach (string dir in dirs)
+                {
+                    _directoryItems.Add(new DirectoryItem
+                    {
+                        Path = dir.Replace("\\", "/"),
+                        Name = Path.GetFileName(dir),
+                        IsSelected = true
+                    });
+                }
             }
         }
     }
