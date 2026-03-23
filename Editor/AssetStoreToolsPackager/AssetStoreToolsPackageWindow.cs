54684145f99556b26f177840fa37775b4b87a386
HIBIKI_5201 <takuto5201@gmail.com>
2026-03-23 17:52:59 +0900

    [add]ウィンドウクラスを追加

---
+++ Editor/AssetStoreToolsPackager/AssetStoreToolsPackageWindow.cs
@@ -0,0 +1,32 @@
+﻿using SymphonyFrameWork.Core;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+namespace SymphonyFrameWork.Editor
+{
+    public class AssetStoreToolsPackageWindow : EditorWindow
+    {
+        public static void ShowWindow()
+        {
+            GetWindow<AssetStoreToolsPackageWindow>(false, "Asset Store Tools Packager", true);
+        }
+
+        private string[] _directories;
+
+        private void OnEnable()
+        {
+            // アセットごとのパスを生成。
+            _directories = Directory.GetDirectories(EditorSymphonyConstant.ASSET_STORE_TOOLS_PATH);
+        }
+
+        private void OnGUI()
+        {
+            GUILayout.Label("Asset Store Tools Packager", EditorStyles.boldLabel);
+            if (GUILayout.Button("Export Asset Store Tools Folder"))
+            {
+                AssetStoreToolsPackager.Export(_directories);
+            }
+        }
+    }
+}
