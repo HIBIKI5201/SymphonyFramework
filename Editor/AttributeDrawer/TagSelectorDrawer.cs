60d8a75dff7d299afbb3b380a3a9de839d438d44
HIBIKI_5201 <takuto5201@gmail.com>
2026-03-25 03:02:57 +0900

    [update]タグセレクターのパラメータをキャッシュ

--- Editor/AttributeDrawer/TagSelectorDrawer.cs
+++ Editor/AttributeDrawer/TagSelectorDrawer.cs
@@ -1,5 +1,6 @@
 ﻿using SymphonyFrameWork.Attribute;
 using System;
+using System.Linq;
 using UnityEditor;
 using UnityEngine;
 
@@ -13,21 +14,33 @@ namespace SymphonyFrameWork.Editor
             // string型のみ対応。
             if (property.propertyType == SerializedPropertyType.String)
             {
-                string[] tags = UnityEditorInternal.InternalEditorUtility.tags;
-
                 // 現在の値のインデックスを探す。
-                int index = Array.IndexOf(tags, property.stringValue);
+                int index = Array.IndexOf(Tags, property.stringValue);
                 if (index < 0) index = 0; // 見つからない場合は最初のタグを選択。
 
                 // プルダウンを表示。
-                int selectedIndex = EditorGUI.Popup(position, label.text, index, tags);
+                int selectedIndex = EditorGUI.Popup(position, label.text, index, Tags);
 
                 // 選択されたタグを文字列として保存。
-                property.stringValue = tags[selectedIndex];
+                property.stringValue = Tags[selectedIndex];
             }
             else
             {
                 EditorGUI.LabelField(position, label.text, "TagSelectorはstring型にのみ使用できます。");
+            }
+        }
+
+        private string[] _tags;
+
+        private string[] Tags
+        {
+            get
+            {
+                if (_tags == null || _tags.Length != EditorBuildSettings.scenes.Length)
+                {
+                    _tags = UnityEditorInternal.InternalEditorUtility.tags;
+                }
+                return _tags;
             }
         }
     }
