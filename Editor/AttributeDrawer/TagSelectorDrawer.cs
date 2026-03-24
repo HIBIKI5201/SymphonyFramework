66e19f5237d662d823aaf0c89417b74546b7274d
HIBIKI_5201 <takuto5201@gmail.com>
2026-03-25 03:38:39 +0900

    [hotfix]バリデーションチェックの比較間違いを修正

--- Editor/AttributeDrawer/TagSelectorDrawer.cs
+++ Editor/AttributeDrawer/TagSelectorDrawer.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Linq;
 using UnityEditor;
+using UnityEditorInternal;
 using UnityEngine;
 
 namespace SymphonyFrameWork.Editor
@@ -12,22 +13,21 @@ namespace SymphonyFrameWork.Editor
         public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
         {
             // string型のみ対応。
-            if (property.propertyType == SerializedPropertyType.String)
-            {
-                // 現在の値のインデックスを探す。
-                int index = Array.IndexOf(Tags, property.stringValue);
-                if (index < 0) index = 0; // 見つからない場合は最初のタグを選択。
-
-                // プルダウンを表示。
-                int selectedIndex = EditorGUI.Popup(position, label.text, index, Tags);
-
-                // 選択されたタグを文字列として保存。
-                property.stringValue = Tags[selectedIndex];
-            }
-            else
+            if (property.propertyType != SerializedPropertyType.String)
             {
                 EditorGUI.LabelField(position, label.text, "TagSelectorはstring型にのみ使用できます。");
+                return;
             }
+
+            // 現在の値のインデックスを探す。
+            int index = Array.IndexOf(Tags, property.stringValue);
+            if (index < 0) index = 0; // 見つからない場合は最初のタグを選択。
+
+            // プルダウンを表示。
+            int selectedIndex = EditorGUI.Popup(position, label.text, index, Tags);
+
+            // 選択されたタグを文字列として保存。
+            property.stringValue = Tags[selectedIndex];
         }
 
         private string[] _tags;
@@ -36,9 +36,9 @@ namespace SymphonyFrameWork.Editor
         {
             get
             {
-                if (_tags == null || _tags.Length != EditorBuildSettings.scenes.Length)
+                if (_tags == null || _tags.Length != InternalEditorUtility.tags.Length)
                 {
-                    _tags = UnityEditorInternal.InternalEditorUtility.tags;
+                    _tags = InternalEditorUtility.tags;
                 }
                 return _tags;
             }
