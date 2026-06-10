ae3ee5d239a6d7a3516e564c73c0aecd0429289f
HIBIKI_5201 <takuto5201@gmail.com>
2026-06-10 17:42:24 +0900

    [add]パッケージのデータを設定できるように

---
+++ Editor/AssetStoreToolsPackager/AssetStoreToolsPackagerData.cs
@@ -0,0 +1,18 @@
+﻿using SymphonyFrameWork.Core;
+using System.Runtime.CompilerServices;
+using UnityEditor;
+
+namespace SymphonyFrameWork.Editor
+{
+    [FilePath(EditorSymphonyConstant.PROJCET_SETTING_FILE_PATH
+        + nameof(AssetStoreToolsPackagerData) + ".asset",
+        FilePathAttribute.Location.ProjectFolder)]
+    public class AssetStoreToolsPackagerData : ScriptableSingleton<AssetStoreToolsPackagerData>
+    {
+        public static string AssetStoreToolsPath => instance._assetStoreToolsPath;
+
+        private string _assetStoreToolsPath = EditorSymphonyConstant.ASSET_STORE_TOOLS_PATH;
+
+        public void Save() => Save(true);
+    }
+}
