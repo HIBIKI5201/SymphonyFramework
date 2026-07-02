e83447947694390c1ab49bbca4d95ce433d5cb9e
HIBIKI_5201 <takuto5201@gmail.com>
2026-07-02 23:38:09 +0900

    [add]サービスロケーターのサンプルを追加

---
+++ Samples/RuntimeSamples/Scripts/ServiceLocatorSample_1.cs
@@ -0,0 +1,17 @@
+﻿using SymphonyFrameWork.System.ServiceLocate;
+using UnityEngine;
+
+namespace SymphonyFrameWork.Samples.ServiceLocatorSample
+{
+    public class ServiceLocatorSample_1 : MonoBehaviour
+    {
+        private Camera _camera;
+
+        private void Start()
+        {
+            ServiceLocator.RegisterInstance(this, LocateType.Singleton);
+
+            _camera = ServiceLocator.GetInstance<Camera>();
+        }
+    }
+}
