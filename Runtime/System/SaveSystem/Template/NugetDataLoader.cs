f7476085d2024a9b9757e276d2ab1560a020378f
HIBIKI_5201 <takuto5201@gmail.com>
2026-07-02 18:25:11 +0900

    [fix]コメントを追加

--- Runtime/System/SaveSystem/Template/NugetDataLoader.cs
+++ Runtime/System/SaveSystem/Template/NugetDataLoader.cs
@@ -5,23 +5,33 @@ using UnityEngine;
 namespace SymphonyFrameWork.System.SaveSystem
 {
     /// <summary>
-    ///     
+    ///     Nugetで導入したJson.NETを使ってデータをセーブするクラス。
     /// </summary>
     /// <typeparam name="T"></typeparam>
     public class NugetDataLoader<T> : ISaveDataLoader<T>
         where T : class, new()
     {
+        /// <summary>
+        ///     データをJson化してセーブする。
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns></returns>
         public ValueTask<SaveData<T>> Save(T data)
         {
             //Json化してセーブ。
             SaveData<T> saveData = new SaveData<T>(data);
             string jsonData = JsonConvert.SerializeObject(saveData);
-            PlayerPrefs.SetString(typeof(T).FullName, jsonData);
+            PlayerPrefs.SetString(typeof(T).FullName, jsonData); // 型のフルネームをキーにしてセーブする。
 
-            Debug.Log($"[{nameof(NugetDataLoader<T>)}]\nデータをセーブしました date : {saveData.SaveDate}\n{data}");
+            Debug.Log($"[{nameof(NugetDataLoader<T>)}]\nデータをセーブしました。 date : {saveData.SaveDate}\n{data}");
             return new(saveData);
         }
 
+        /// <summary>
+        ///     データをロードする。
+        ///     データがない場合は新しいインスタンスを生成する。
+        /// </summary>
+        /// <returns></returns>
         public ValueTask<SaveData<T>> Load()
         {
             #region Prefsからデータをロードする
@@ -29,7 +39,7 @@ namespace SymphonyFrameWork.System.SaveSystem
             string json = PlayerPrefs.GetString(typeof(T).FullName);
             if (string.IsNullOrEmpty(json))
             {
-                Debug.Log($"[{nameof(NugetDataLoader<T>)}]\n{typeof(T).Name}のデータが見つからないので生成しました");
+                Debug.Log($"[{nameof(NugetDataLoader<T>)}]\n{typeof(T).Name}のデータが見つからないので生成しました。");
                 return new(new SaveData<T>(new T()));
             }
 
@@ -41,11 +51,11 @@ namespace SymphonyFrameWork.System.SaveSystem
 
             if (data == null)
             {
-                Debug.LogWarning($"[{nameof(NugetDataLoader<T>)}]\n{typeof(T).Name}のロードが出来ませんでした\n新たなインスタンスを生成します");
+                Debug.LogWarning($"[{nameof(NugetDataLoader<T>)}]\n{typeof(T).Name}のロードに失敗しました。\n新たなインスタンスを生成します。");
                 return new(new SaveData<T>(new T()));
             }
 
-            Debug.Log($"[{nameof(NugetDataLoader<T>)}]\n{typeof(T).Name}のデータがロードされました\n{data}");
+            Debug.Log($"[{nameof(NugetDataLoader<T>)}]\n{typeof(T).Name}のデータがロードされました。\n{data}");
             return new(data);
 
             #endregion
