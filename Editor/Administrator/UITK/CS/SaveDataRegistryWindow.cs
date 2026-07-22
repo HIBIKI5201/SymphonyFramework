4dcd81376a81f6b77c6a90bcb2d985ab85719f70
HIBIKI_5201 <takuto5201@gmail.com>
2026-07-22 12:54:33 +0900

    [fix]usingを修正

--- Editor/Administrator/UITK/CS/SaveDataRegistryWindow.cs
+++ Editor/Administrator/UITK/CS/SaveDataRegistryWindow.cs
@@ -3,6 +3,7 @@ using SymphonyFrameWork.Utility;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
