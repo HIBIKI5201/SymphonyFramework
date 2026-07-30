bc55d5576f7f16ce1f2e0bc715ee9e21456b5cb3
HIBIKI_5201 <takuto5201@gmail.com>
2026-07-31 06:29:38 +0900

    [fix]ReadOnlyがMonoBehaviorのドロワーを上書きしていた問題を修正

--- Editor/AttributeDrawer/ReadOnlyDrawer.cs
+++ Editor/AttributeDrawer/ReadOnlyDrawer.cs
@@ -1,5 +1,4 @@
-﻿using System.Reflection;
-using UnityEditor;
+﻿using UnityEditor;
 using UnityEngine;
 
 namespace SymphonyFrameWork.Attribute
@@ -17,34 +16,6 @@ namespace SymphonyFrameWork.Attribute
             {
                 EditorGUI.PropertyField(position, property, label, true);
             }
-        }
-    }
-
-    /// <summary>
-    ///     ReadOnryが表示されない場合に警告を出す
-    /// </summary>
-    [CustomEditor(typeof(MonoBehaviour), true)]
-    public sealed class ReadOnlyInspector : UnityEditor.Editor
-    {
-        /// <summary> ReadOnly属性のシリアライズ漏れを警告して既定Inspectorを描画する。 </summary>
-        public override void OnInspectorGUI()
-        {
-            serializedObject.Update();
-
-            var targetType = target.GetType();
-            var fields = targetType.GetFields(BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance);
-
-            foreach (var field in fields)
-                if (field.IsDefined(typeof(ReadOnlyAttribute), true))
-                {
-                    var property = serializedObject.FindProperty(field.Name);
-                    if (property == null)
-                        Debug.LogWarning(
-                            $"フィールド '{field.Name}' は [ReadOnly] 属性が付与されていますが、[SerializeField] 属性が付与されていないため、インスペクターに表示されません。");
-                }
-
-            DrawDefaultInspector();
-            serializedObject.ApplyModifiedProperties();
         }
     }
 }
